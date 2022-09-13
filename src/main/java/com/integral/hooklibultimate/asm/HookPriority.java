ab70fa1e9dcbdc701323795208d0a1587838bfed
georgii.imeshkenov <georgii.imeshkenov@mati.io>
2022-09-13 18:50:30 +0300

    added embedded lib & appropriate suffix

---
+++ src/main/java/com/integral/hooklibultimate/asm/HookPriority.java
@@ -0,0 +1,11 @@
+package com.integral.hooklibultimate.asm;
+
+public enum HookPriority {
+
+    HIGHEST, // Вызывается первым
+    HIGH,
+    NORMAL,
+    LOW,
+    LOWEST // Вызывается последним
+
+}
